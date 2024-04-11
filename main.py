092ca2a476d50179909d1fd4cb68814d47158e46
MitchDJ <mitjohn@chbc.school.nz>
2024-04-12 11:51:25 +1200

    Finished

---
+++ main.py
@@ -0,0 +1,78 @@
+import random
+
+all_question_types = ["+", "-", "x", "/"]
+
+score = 0
+first_number = random.randrange(1, 20)
+second_number = random.randrange(1, 20)
+question_number = 0
+question_count = 0
+final_question_count = 0
+
+def int_checker(question):
+    while True:
+        try:
+            return int(input(question))
+        except:
+            print("Please enter a valid number")
+
+
+def one_question():
+    global question_types, score
+    question_types = random.choice(all_question_types)
+    first_number = random.randrange(1, 20)
+    second_number = random.randrange(1, 20)
+    correct_answer = 0.0
+
+    if question_types == "+":
+        correct_answer = first_number + second_number
+    elif question_types == "-":
+        correct_answer = first_number - second_number
+    elif question_types == "x":
+        correct_answer = first_number * second_number
+    elif question_types == "/":
+        correct_answer = first_number / second_number
+    correct_answer = round(correct_answer, 1)
+    question = "What is {} {} {}? (rounded to the first decimal place)\n".format(first_number, question_types,
+                                                                                 second_number)
+    user_answer = float(input(question))
+    user_answer = round(user_answer, 1)
+    if user_answer == round(correct_answer, 1):
+        print("Correct!")
+        score += 1
+    else:
+        print(f"Incorrect! The correct answer was {correct_answer}")
+def main():
+    global question_number, question_count, final_question_count
+    while True:
+        game = input("Are you ready to play? (Y/N)\n").capitalize()
+        if game == "Yes" or game == "Y":
+            question_count = question_number + int_checker(f"How many questions do you want {username}?\n")
+            final_question_count += question_count
+            print("Alrighty Then Starting Quiz")
+            break
+
+        elif game == "No" or game == "N":
+            print("Too Bad you playing")
+        else:
+            print("Bro put a valid answer")
+
+    for question_number in range(question_count):
+        print(f"Question {question_number + 1}")
+        one_question()
+
+    print("Well done")
+    print(f"You Got {score} / {final_question_count} correct!")
+
+print("~~~~~Math Quiz~~~~~")
+username = input("Enter your name please\n").capitalize()
+print("Welcome to the game {}".format(username))
+
+main()
+
+replay = input("Would you like to replay? (Y/N)\n").upper()
+if replay == "YES" or replay == "Y":
+    main()
+elif replay == "NO" or replay == "N":
+    print("Thank you for playing")
+
